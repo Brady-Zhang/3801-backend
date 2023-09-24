18f26327735ba29e72bc18757cd6df419de2bf98
depeng zhang <zhangdepeng3@gmail.com>
2023-09-24 17:43:51 +1000

    add user name post router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = mongoose.model('users', new mongoose.Schema({
+  fullName: String
+}));
+
+//http://localhost:3000/users/addUser
+router.post('/addUser', async (req, res) => {
+  const { fullName } = req.body;
+  try {
+      const name = new User({ fullName });
+      await name.save();
+      res.status(200).send({ status: 'success', message: 'Name saved successfully' });
+  } catch (error) {
+      res.status(500).send({ status: 'error', message: 'Server Error' });
+  }
 });
+
 
 module.exports = router;
