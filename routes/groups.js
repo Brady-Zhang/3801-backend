1970cefe1c72cd0cc0a0a5e1a13b14daf857a6a1
depeng zhang <zhangdepeng3@gmail.com>
2023-09-24 19:06:10 +1000

    groups routes /groups/createGroup

--- routes/groups.js
+++ routes/groups.js
@@ -1,9 +1,23 @@
+// routes/group.js
 var express = require('express');
 var router = express.Router();
 
-/* GET groups listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Group = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/models/groups.js');
+const User = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/models/users.js');
+
+router.post('/createGroup', async (req, res) => {
+  const { name, userId } = req.body;
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(400).send({ message: 'User not found' });
+
+    const group = new Group({ name, members: [user._id] });
+    await group.save();
+    res.status(200).send({ message: 'Group created successfully', group });
+  } catch (error) {
+    res.status(500).send({ message: 'Server Error' });
+  }
 });
 
 module.exports = router;
+
