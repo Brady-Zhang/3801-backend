0ed7901c8eea6f0624dd8387e29f77f4ccfe69cf
depeng zhang <zhangdepeng3@gmail.com>
2023-10-03 17:18:28 +1000

    comment on Group

--- models/groups.js
+++ models/groups.js
@@ -5,7 +5,7 @@ const GroupSchema = new mongoose.Schema({
   name: String,
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
-
+// Group is a model based on the schema(GroupSchema)
 const Group = mongoose.model('Group', GroupSchema);
 
 module.exports = Group;
