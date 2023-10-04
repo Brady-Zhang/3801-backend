985c59c7d03a68fa4c4e9119eeae640d1c10d3e9
depeng zhang <zhangdepeng3@gmail.com>
2023-10-04 14:12:26 +1000

    add JWT to group routes

--- routes/groups.js
+++ routes/groups.js
@@ -5,19 +5,49 @@ var router = express.Router();
 const Group = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/models/groups.js');
 const User = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/models/users.js');
 
-router.post('/createGroup', async (req, res) => {
-  const { name, userId } = req.body;
-  try {
-    const user = await User.findById(userId);
-    if (!user) return res.status(400).send({ message: 'User not found' });
+const authenticateJWT = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/middleware/authentication.js');
 
-    const group = new Group({ name, members: [user._id] });
-    await group.save();
-    res.status(200).send({ message: 'Group created successfully', group });
-  } catch (error) {
-    res.status(500).send({ message: 'Server Error' });
-  }
+router.post('/createGroup', authenticateJWT, async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).send({ status: 'error', message: 'Group name is required' });
+    }
+
+    // 确保 req.user 已被身份验证中间件设置
+    if (!req.user || !req.user._id) {
+        return res.status(401).send({ status: 'error', message: 'User not authenticated' });
+    }
+
+    try {
+        const group = new Group({
+            name: name,
+            members: [req.user._id] // 自动将创建者的 userId 添加到 members 中
+        });
+
+        await group.save();
+        res.status(200).send({ status: 'success', message: 'Group created successfully', group });
+    } catch (error) {
+        res.status(500).send({ status: 'error', message: 'Server Error' });
+    }
 });
+
+module.exports = router;
+
+
+// router.post('/createGroup', async (req, res) => {
+//   const { name} = req.body;
+//   try {
+//     const user = await User.findById(userId);
+//     if (!user) return res.status(400).send({ message: 'User not found' });
+
+//     const group = new Group({ name, members: [user._id] });
+//     await group.save();
+//     res.status(200).send({ message: 'Group created successfully', group });
+//   } catch (error) {
+//     res.status(500).send({ message: 'Server Error' });
+//   }
+// });
 
 
 router.post('/addMemberToGroup', async (req, res) => {
