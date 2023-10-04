dac36a024f51b1db55c14d41dd7e57b06d43dd91
depeng zhang <zhangdepeng3@gmail.com>
2023-10-04 14:12:46 +1000

    add JWT to users route

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,41 @@
 var express = require('express');
 var router = express.Router();
 const User = require('/Users/depengzhang/Documents/UQ/s2-2023/DECO3801/3801-backend/models/users.js');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "YOUR_SECRET_KEY"; 
 
 //http://localhost:3000/users/addUser
 router.post('/addUser', async (req, res) => {
   const { fullName } = req.body;
   try {
-    //creat a new User with the extracted name
-      const name = new User({ fullName });
-      //asynchronously save
-      await name.save();
-      //200 means ok, request is successful. send back a JSON
-      res.status(200).send({ status: 'success', message: 'Name saved successfully' });
+    // 检查用户名是否已存在
+    const existingUser = await User.findOne({ fullName });
+    if (existingUser) {
+      return res.status(400).send({ status: 'error', message: 'Name already exists' });
+    }
+
+    // 创建一个新的用户
+    const user = new User({ fullName });
+    await user.save();
+
+    // 生成JWT
+    const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: '1h' });
+
+    // 返回JWT和成功消息
+    res.status(200).send({
+      status: 'success', 
+      message: 'Name saved successfully', 
+      token: token // 返回JWT给客户端
+    });
   } catch (error) {
       res.status(500).send({ status: 'error', message: 'Server Error' });
   }
 });
 
 module.exports = router;
+
+
+module.exports = router;
+
+
+
